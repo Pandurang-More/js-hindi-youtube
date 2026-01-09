fb2bc04fcdb253ff0e15ffa1c3254e55dea3ee0f
Pandurang More <morepandurang264@gmail.com>
2026-01-09 23:53:27 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "hitesh"
 
-//console.log(typeof score);
-//console.log(typeof(score));
+//console.log(typeof score);     string
+//console.log(typeof(score);      string
 
 let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+//console.log(typeof valueInNumber);     number
+//console.log(valueInNumber);            NAN
 
 
 // "33" => 33
@@ -24,14 +24,20 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-// console.log(stringNumber);
-// console.log(typeof stringNumber);
+// console.log(stringNumber);         string
+// console.log(typeof stringNumber);  NAN
+
+
+//NaN stands for “Not a Number”, but:
+
+👉 Its data type is Number
+// It represents a failed or invalid numeric operation.
 
 // *********************** Operations ***********************
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+// console.log(negValue);   -3
 
 // console.log(2+2);
 // console.log(2-2);
@@ -44,16 +50,16 @@ let str1 = "hello"
 let str2 = " hitesh"
 
 let str3 = str1 + str2
-// console.log(str3);
+// console.log(str3);   hello hitesh
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+// console.log("1" + 2);   12
+// console.log(1 + "2");   12
+// console.log("1" + 2 + 2);  12
+// console.log(1 + 2 + "2");  32
 
 // console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
+// console.log(+true);1
 // console.log(+"");
 
 let num1, num2, num3
