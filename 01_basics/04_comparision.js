272e0dfa95f3bd4cc682011d4407b67d5ed32e78
Pandurang More <morepandurang264@gmail.com>
2026-01-10 18:53:22 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,21 +1,48 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+console.log(2 > 1);      //true
+console.log(2 >= 1);     //true
+ 
+console.log(2 < 1);         //false
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);        // true
+console.log("02" > 1);      // true
+<------------------------------------------------------------------------------------------------------->
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+//null → converted to 0 in relational comparison (>=, <=)
 
-console.log(undefined == 0);
-console.log(undefined > 0);
+//== does NOT convert null to number
+
+console.log(null > 0);      //false
+console.log(null == 0);     // false      
+console.log(null >= 0);     // true        
+<------------------------------------------------------------------------------------------------------->
+
+//undefined becomes NaN → all comparisons are false
+
+console.log(undefined == 0);  
+console.log(undefined > 0);     
 console.log(undefined < 0);
 
-// === 
+<------------------------------------------------------------------------------------------------------->
+console.log("2" === 2);    // false     // it first check the datatype then compare, strict checking
 
-console.log("2" === 2);
+
+== → compares value only (type conversion happens)
+
+=== → compares value + type (no conversion)
+
+!= → value comparison
+
+!== → value + type comparison
+
+
+null == undefined   // true
+null === undefined  // false
+<------------------------------------------------------------------------------------------------------->
+typeof null       // "object"  ❗ (JS bug)
+typeof undefined  // "undefined"
+
+
+null + 1        // 1
+undefined + 1   // NaN
+undefined and null both are falsy value.
