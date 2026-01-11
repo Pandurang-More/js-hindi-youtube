7e32c178df542e89edf084f7bc751ad103202298
Pandurang More <morepandurang264@gmail.com>
2026-01-11 11:45:14 +0530

    Update 06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,22 +1,31 @@
 const score = 400
-// console.log(score);
+ console.log(score);
 
 const balance = new Number(100)
-// console.log(balance);
+ console.log(balance);
 
-// console.log(balance.toString().length);
-// console.log(balance.toFixed(1));
-
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+output:
+400
+[Number: 100]
+3
+100.0
+<------------------------------------------------------------------------------------------------------------------------------->
 const otherNumber = 123.8966
 
-// console.log(otherNumber.toPrecision(4));
+console.log(otherNumber.toPrecision(5));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN'));
+
+output:
+123.90
+10,00,000
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
-// console.log(Math);
+// console.log(Math);           // Object [Math] {}
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
@@ -27,8 +36,13 @@ const hundreds = 1000000
 console.log(Math.random());
 console.log((Math.random()*10) + 1);
 console.log(Math.floor(Math.random()*10) + 1);
+output:
+0.5407525630986403  // in between 0 and 1
+5.007562930689175   // in between 1 and 10
+9                   // in between 1 and 10 but with precision
+
 
 const min = 10
 const max = 20
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)  // generate random number between 10 and 20
