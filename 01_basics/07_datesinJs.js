c7161c1bbfa242e3079a01483b210956f2ac8d2e
Pandurang More <morepandurang264@gmail.com>
2026-01-11 12:55:08 +0530

    Update 07_datesinJs.js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,27 +1,42 @@
 // Dates
 
-let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+let MyDate=new Date()
+console.log(MyDate.toString())
+console.log(MyDate.toDateString())
+console.log(MyDate.toLocaleString())
+console.log(typeof(MyDate))
+op:
+Sun Jan 11 2026 12:07:35 GMT+0530 (India Standard Time)
+Sun Jan 11 2026
+11/1/2026, 12:07:35 pm
+object
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
-// let myCreatedDate = new Date("2023-01-14")
+
 let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString());   
+
+op: 14/1/2023, 12:00:00 am
+
+
 
 let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(Math.floor(Date.now()/1000));
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+op:
+1768113794438        miliseconds
+1768113794           seconds
+
 
 let newDate = new Date()
 console.log(newDate);
 console.log(newDate.getMonth() + 1);
 console.log(newDate.getDay());
+
+op:
+2026-01-11T06:45:22.426Z
+1
+0
 
 // `${newDate.getDay()} and the time `
 
